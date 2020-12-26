6a99023f2832fb027f64c1edf96fd85c0f9fc1cb
Zekai Huang <zekaihuang92@gmail.com>
2020-12-26 18:36:20 +0100

    plot3

---
+++ plot3.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+dat <- read_delim("household_power_consumption.txt",col_names =TRUE,delim =";",na ="?")
+dat
+library(lubridate)
+dat$Date <-  dmy(dat$Date)
+dat
+dat1 <- dat %>% 
+        filter(Date =="2007-02-01" | Date =="2007-02-02")
+dat1
+
+dat2 <- dat1 %>% 
+        mutate(Date_time=paste(Date,Time))
+dat2$Date_time <- ymd_hms(dat2$Date_time)
+dat2
+
+with(dat2, {
+        plot(Sub_metering_1~Date_time, type="l",
+             ylab="Global Active Power (kilowatts)", xlab="")
+        lines(Sub_metering_2~Date_time,col='Red')
+        lines(Sub_metering_3~Date_time,col='Blue')
+})
+
+legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
